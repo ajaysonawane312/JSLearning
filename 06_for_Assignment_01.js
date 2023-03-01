159f85b2a9b0ff7df085da545691e476daed85b1
ajaysonawane312 <ajaysonawane312@gmail.com>
2023-03-01 09:31:33 +0530

    Using for---Loop Assignment Completed
    Using for---Loop Assignment Completed

---
+++ 06_for_Assignment_01.js
@@ -0,0 +1,51 @@
+console.log(`Print the numbers 5 to 15 Incrementing By 1:`);
+for (let index = 5; index <=15; index++) {
+    console.log (`${index}`); 
+    
+}
+console.log(`\n`);
+console.log(`Print numbers from 50 to 40 Decrementing By 1:`);
+for ( i = 50; i>=40; i--) {
+    console.log(`${i}`);
+    
+}
+console.log(`\n`);
+console.log(`Find first 15 odd numbers :`);
+for (num=1;num<=30; num++) 
+{
+ if (num%2!=0) {
+    console.log(`${num}`); 
+ }    
+    
+}
+
+console.log(`\n`);
+console.log(`Find first 15 even numbers :`);
+for (num=1;num<=30; num++) 
+{
+ if (num%2==0) {
+    console.log(`${num}`); 
+ }    
+    
+}
+console.log(`\n`);
+console.log(`Print table of 5 to 50:`);
+for (let index =5; index<=50; index+=5) {
+    if (index) {
+        console.log(`${index}`);
+    }
+    }
+    console.log(`\n`);
+    console.log(`Print table of 10 to 100:`);
+for (let index =10; index<=100; index+=10) {
+    if (index) {
+        console.log(`${index}`);
+    }
+    }
+    console.log(`\n`);
+    console.log(`Print table of Reverse 100 to 10:`);
+for (let index =100; index>=10; index-=10) {
+    if (index) {
+        console.log(`${index}`);
+    }
+    }
